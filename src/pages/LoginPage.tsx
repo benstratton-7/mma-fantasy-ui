b4f12007fadb8940a86a5f850c478f212244d49c
Ben Stratton <bennstratton7@gmail.com>
2025-01-18 13:57:54 -0500

    login style
    forgot to add style to login. quick and easy centering

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,17 +3,19 @@ import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
-    const { login } = useAuth();
-    const  navigate = useNavigate();
-    const handleLogin = () => {
-        login();
-        navigate("/home")
-    }
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    login();
+    navigate('/home');
+  };
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Please log in to access the app.</p>
-      <button onClick={handleLogin}>Log In</button>
+    <div className="loginPage">
+      <div className="loginContainer">
+        <h1>Login</h1>
+        <p>Please log in to access the app.</p>
+        <button onClick={handleLogin}>Log In</button>
+      </div>
     </div>
   );
 };
